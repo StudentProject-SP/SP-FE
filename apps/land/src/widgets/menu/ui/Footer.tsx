041abb29d55499da3d71a8ec17094db04588d230
cho4u4o <cho4u4o@gmail.com>
2025-02-12 01:53:35 +0900

    feat(land): make footer and navigation bar

---
+++ apps/land/src/widgets/menu/ui/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="h-51 bg-sub box-border flex w-full items-center justify-center p-4">
+      <div className="flex w-[60%] justify-between">
+        <div className="text-text-2 flex-col">
+          <p className="mb-4">Soup</p>
+          <p>Designed by Freepik</p>
+          <a href="www.freepik.com">
+            <u>www.freepik.com</u>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ apps/land/src/widgets/menu/ui/Nav.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Nav() {
+  return (
+    <nav className="h-13 box-border flex w-full items-center justify-center p-4">
+      <div className="flex w-[60%] justify-between">
+        <Image alt="logo" src="/images/soup.png" width={57} height={30} />
+        <p className="text-text-2 hover:text-text-1 cursor-pointer text-sm">
+          로그인/회원가입
+        </p>
+      </div>
+    </nav>
+  );
+}
