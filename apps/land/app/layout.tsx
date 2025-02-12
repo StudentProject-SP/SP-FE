97ad76b509aa6269fdba152497dbbb94f5df6cdc
Jeong-Ag <kjungu1105@gmail.com>
2025-02-12 14:13:11 +0900

    refactor(land): apply code formatting

--- apps/land/app/layout.tsx
+++ apps/land/app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Soup",
-  description: "",
+  title: 'Soup',
+  description: '',
 };
 
 export default function RootLayout({
