213dde25eb2d2212078ad0d8d5036d27fcf0de9a
cho4u4o <cho4u4o@gmail.com>
2025-02-12 16:52:17 +0900

    refactor(land): change color name

--- apps/land/src/widgets/menu/ui/Footer.tsx
+++ apps/land/src/widgets/menu/ui/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="h-51 bg-sub box-border flex w-full items-center justify-center p-4">
       <div className="flex w-[60%] justify-between">
-        <div className="text-text-2 flex-col">
+        <div className="text-text-light flex-col">
           <p className="mb-4">Soup</p>
           <p>Designed by Freepik</p>
           <a href="www.freepik.com">
--- apps/land/src/widgets/menu/ui/Nav.tsx
+++ apps/land/src/widgets/menu/ui/Nav.tsx
@@ -6,7 +6,7 @@ export default function Nav() {
     <nav className="h-13 box-border flex w-full items-center justify-center p-4">
       <div className="flex w-[60%] justify-between">
         <Image alt="logo" src="/images/soup.png" width={57} height={30} />
-        <p className="text-text-2 hover:text-text-1 cursor-pointer text-sm">
+        <p className="text-text-light hover:text-text-dark cursor-pointer text-sm">
           로그인/회원가입
         </p>
       </div>
